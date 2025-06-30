8f4d6f4e302819285532e68b033f2624bbf9fa49
221fa04732 <221fa04732@gmail.com>
2025-06-30 15:40:08 +0530

    delete supplier api added

--- backend/src/handler/supplier.ts
+++ backend/src/handler/supplier.ts
@@ -2,7 +2,9 @@ import express from 'express'
 const supplierRouter = express.Router();
 
 import addSupplier from '../routes/addSupplier';
+import deleteSupplier from '../routes/deleteSupplier';
 
 supplierRouter.post('/add', addSupplier)
+supplierRouter.post('/delete', deleteSupplier)
 
 export default supplierRouter;
---
+++ backend/src/routes/deleteSupplier.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "../../src/generated/prisma";
+
+const prisma= new PrismaClient()
+
+const deleteSupplier = (async(req : Request, res : Response)=>{
+    const {id} : {id: string} = req.body;
+    try{
+        await prisma.supplier.update({
+            data : {
+                delete : true
+            },
+            where : {
+                id : id
+            }
+        })
+        res.status(201).json({
+            msg : "Supplier deleted"
+        })
+    }
+    catch(e){
+        console.error(e);
+        res.status(500).json({
+            msg: "Server error"
+        })
+    }
+})
+
+export default deleteSupplier;
