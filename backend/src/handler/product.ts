fe8450fdfc715149795ef2306d3ae908f91a7aa5
221fa04732 <221fa04732@gmail.com>
2025-06-30 13:55:18 +0530

    get product with search param and pagization done

--- backend/src/handler/product.ts
+++ backend/src/handler/product.ts
@@ -4,9 +4,11 @@ const productRouter = express.Router();
 import addProduct from '../routes/addProduct';
 import deleteProduct from '../routes/deleteProduct';
 import updateProduct from '../routes/updateProduct';
+import getProduct from '../routes/getProduct';
 
 productRouter.post('/add', addProduct)
 productRouter.post('/delete', deleteProduct)
 productRouter.post('/update', updateProduct)
+productRouter.get('/', getProduct)
 
 export default productRouter;
---
+++ backend/src/routes/getProduct.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "../../src/generated/prisma";
+
+const prisma= new PrismaClient()
+
+const getProduct = (async(req : Request, res : Response)=>{
+    const page : number = Number(req.query.page)
+    const searchWord : string = req.query.search as string
+    console.log(page)
+    console.log(searchWord)
+    try{
+        const limit= 2;
+        const skip= limit*page
+        const product = await prisma.product.findMany({
+            where : {
+                delete : false,
+                OR:[{
+                    name : {
+                        contains : searchWord,
+                        mode : "insensitive"
+                    },
+                },{
+                    category : {
+                        contains : searchWord,
+                        mode: "insensitive"
+                    }
+                }]
+            },
+            skip: skip,
+            take: limit
+        })
+
+        const toalProduct = await prisma.product.count({
+            where : {
+                delete : false,
+                OR:[{
+                    name : {
+                        contains : searchWord,
+                        mode : "insensitive"
+                    },
+                },{
+                    category : {
+                        contains : searchWord,
+                        mode: "insensitive"
+                    }
+                }]
+            }
+        })
+        const hasPreviousPage= page>0
+        const hasNextPage= page<Math.ceil(toalProduct/limit)-1
+        res.status(201).json({
+            product,
+            hasNextPage,
+            hasPreviousPage
+        })
+
+    }
+    catch(e){
+        console.error(e);
+        res.status(500).json({
+            msg: "Server error"
+        })
+    }
+})
+
+export default getProduct;
