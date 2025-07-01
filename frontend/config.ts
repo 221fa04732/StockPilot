c52c31a30aa854a31bd47cfda166fd6a1ecef902
221fa04732 <221fa04732@gmail.com>
2025-07-01 19:54:11 +0530

    backend url updated

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,3 +1,3 @@
 // export const BACKEND_URL="http://localhost:8000"
 
-export const BACKEND_URL="ec2-52-66-249-30.ap-south-1.compute.amazonaws.com:8000"
+export const BACKEND_URL="http://ec2-52-66-249-30.ap-south-1.compute.amazonaws.com:8000"
