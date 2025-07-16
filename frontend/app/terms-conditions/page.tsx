0d8350ba06c36a222922f5311ab1a86a1d04d269
221fa04732 <221fa04732@gmail.com>
2025-07-16 20:28:53 +0530

    added empty terms and conditions page route

---
+++ frontend/app/terms-conditions/page.tsx
@@ -0,0 +1,5 @@
+export default function TermsConditions(){
+    return(<div className="bg-gray-950 min-h-screen w-full flex justify-center items-center text-2xl text-white font-bold">
+        Terms & Conditions
+    </div>)
+}
