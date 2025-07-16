8e32d6707885aa0b947585c79a085eb3229f58f6
221fa04732 <221fa04732@gmail.com>
2025-07-16 22:42:43 +0530

    report dummy  route done

---
+++ frontend/app/report/page.tsx
@@ -0,0 +1,5 @@
+export default function Report(){
+    return(<div className="bg-gray-950 min-h-screen w-full flex justify-center items-center text-2xl text-white font-bold">
+        Report
+    </div>)
+}
